4d4fff67da14c299c1c92980b424dc4f79c39639
Guilherme <164170606+Guilhermejrf@users.noreply.github.com>
2026-04-30 07:44:35 -0300

    Atualizar o app.js

--- MinhaAula/app.js
+++ MinhaAula/app.js
@@ -1,5 +1,5 @@
 // 1. Importação das bibliotecas
-import { initializeApp } from "firebase/app"; 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 // 2. Sua configuração do firebase
