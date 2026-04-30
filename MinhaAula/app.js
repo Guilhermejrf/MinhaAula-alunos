f3fb6c6bf3b151c2faee6300e2a495567e7f1e01
Guilherme <164170606+Guilhermejrf@users.noreply.github.com>
2026-04-29 23:54:28 -0300

    Refactor Firebase initialization and user registration
    Updated Firebase imports and configuration details.

--- MinhaAula/app.js
+++ MinhaAula/app.js
@@ -1,21 +1,33 @@
-// 1.
-// Sem o link https://... o navegador não encontra o Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// 1. Importação das bibliotecas
+import { initializeApp } from "firebase/app"; 
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// 2. Sua configuração (Mantenha como está, mas dentro deste arquivo novo)
+// 2. Sua configuração do firebase
 const firebaseConfig = {
-  apiKey: "Sua apiKey",
-  authDomain: "Seu authDomain",
-  projectId: "Seu projectId",
-  storageBucket: "Seu storageBucket",
-  messagingSenderId: "Seu messagingSenderId",
-  appId: "appId",
-  measurementId: "measurementId"
+  apikey: "AIzaSyCLuFQ5N3xo0noEf9NlfgoH7jBTKtJY1OA", 
+  authDomain: "aulateste-3bafc.firebaseapp.com",
+  projectId: "aulateste-3bafc",
+  storageBucket: "aulateste-3bafc.firebasestorage.app",
+  messagingSenderId: "517061844117",
+  appId: "1:517061844117:web:e95ab42a03a3f769180f9c"
 };
 
-// 3. Inicialização dos coiso
+// 3. Inicialização das configurações
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// 4. Função de Cadastro (Usando window. para o HTML enxergar)
+// 4. Função de Cadastro
+function cadastrarComEmail() {
+    // Pegar os valores que o usuário digitou
+    const email = document.getElementById('e-mail').value; 
+    const password = document.getElementById('password').value;
+
+    // O que vai enviar para o Firebase criar a conta
+    createUsersWithEmailAndPassword(auth, email, password) 
+        .then((userCredential) => {
+            alert("Sucesso! Usuário: " + userCredential.user.email);
+        })
+        .catch((error) => {
+            alert("Erro: " + error.message);
+        });
+}
