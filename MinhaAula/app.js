54aba8fe84329638f3a08c7c87c122fa8e843dd7
Guilherme <164170606+Guilhermejrf@users.noreply.github.com>
2026-04-29 21:06:04 -0300

    Update Firebase configuration placeholders in app.js

--- MinhaAula/app.js
+++ MinhaAula/app.js
@@ -5,13 +5,13 @@ import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com
 
 // 2. Sua configuração (Mantenha como está, mas dentro deste arquivo novo)
 const firebaseConfig = {
-  apiKey: "AIzaSyCAQjdjCWPMv3bU0rzg6H_MaWg583vv5ug",
-  authDomain: "aula-1-guilherme.firebaseapp.com",
-  projectId: "aula-1-guilherme",
-  storageBucket: "aula-1-guilherme.firebasestorage.app",
-  messagingSenderId: "486984790863",
-  appId: "1:486984790863:web:daf013d0a813a712ff4b3a",
-  measurementId: "G-PV7JRYVEBG"
+  apiKey: "Sua apiKey",
+  authDomain: "Seu authDomain",
+  projectId: "Seu projectId",
+  storageBucket: "Seu storageBucket",
+  messagingSenderId: "Seu messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId"
 };
 
 // 3. Inicialização dos coiso
